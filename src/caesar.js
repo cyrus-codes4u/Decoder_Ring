f22fc62cfb289613cb962efb8a91d3cf0551029d
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-01-03 22:55:44 -0500

    caesar.js wrote function caesar

--- src/caesar.js
+++ src/caesar.js
@@ -1,3 +1,17 @@
-function caesar(input, shift, encode = true) {}
+function caesar(input, shift, encode = true) {
+    if(!input || shift === 0 || shift > 25 || shift < -25) {return false}
+    
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let message = input.toLowerCase();
+    for (let char in message){
+        const character = message[char];
+        if(alphabet.includes(character)){
+            let shiftedIndex = alphabet.indexOf(character);
+            encode ? shiftedIndex += shift : shiftedIndex -= shift;
+            message[char] = alphabet[shiftedIndex];
+        }
+    }
+    return message
+}
 
 module.exports = caesar;
